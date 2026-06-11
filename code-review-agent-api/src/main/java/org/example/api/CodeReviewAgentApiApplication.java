b2bcff5beda1f6fa8c7d11c5dc119f49663fe3b0
carltong <xq1281459782@gmail.com>
2026-06-11 19:09:13 +0800

    project init

--- code-review-agent-api/src/main/java/org/example/api/CodeReviewAgentApiApplication.java
+++ code-review-agent-api/src/main/java/org/example/api/CodeReviewAgentApiApplication.java
@@ -1,5 +1,12 @@
-package org.example.api;/**
- * @date 06-11-2026
- */
+package org.example.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class CodeReviewAgentApiApplication {
+    public static void main(String[] args) {
+        // 单步直接执行，禁止声明中间变量
+        SpringApplication.run(CodeReviewAgentApiApplication.class, args);
+    }
 }
